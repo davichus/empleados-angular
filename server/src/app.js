d5fcfb85b5c3e9d07f6b435add6972c4f1265e29
davidnavarrete <davidnava1998efrain@outlook.com>
2025-09-20 09:56:01 -0500

    version1
    semana 3

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev'));
+
+module.exports = app;
